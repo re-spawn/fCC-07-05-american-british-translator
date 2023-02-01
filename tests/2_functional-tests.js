31cfb90d8bec29ff8e293b6d51ff98a0c4339434
re-spawn <rob@robanthony.ca>
2023-02-01 15:22:57 -0330

    chore: (more) setup

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,7 +1,14 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
-const server = require('../server.js');
+
+// const server = require('../server.js');
+require('dotenv').config();
+if (process.env.HOST === 'repl') {
+  var server = 'http://fCC-07-05-american-british-translator.respawn709.repl.co';
+} else {
+  var server = require('../server.js');
+}
 
 chai.use(chaiHttp);
 
